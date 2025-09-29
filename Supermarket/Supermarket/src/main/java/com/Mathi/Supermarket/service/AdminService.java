039ab3eddad039b53eaf24c05a495319d93df3f9
Sathushan18 <jhcsathu09@gmail.com>
2025-09-29 17:37:47 +0530

    Add Admin Login Logic

---
+++ Supermarket/Supermarket/src/main/java/com/Mathi/Supermarket/service/AdminService.java
@@ -0,0 +1,27 @@
+package com.Mathi.Supermarket.service;
+
+import com.Mathi.Supermarket.model.Admin;
+import com.Mathi.Supermarket.repository.AdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AdminService {
+
+    @Autowired
+    private AdminRepository adminRepository;
+
+
+    // --- Check admin login credentials ---
+    public boolean checkLogin(String username, String password) {
+        Optional<Admin> adminOptional = adminRepository.findByUsername(username);
+        if (adminOptional.isPresent()) {
+            Admin admin = adminOptional.get();
+            return admin.getPassword().equals(password); // simple password check
+        }
+        return false;
+    }
+
+}
