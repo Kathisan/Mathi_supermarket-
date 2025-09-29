d23b2a21a9390b9966b263b78f85dc54fcb9fbb7
kaneswaran-varun <kaneswaranvarun@gmail.com>
2025-09-29 20:24:45 +0530

    add profile endpoints (get user by username, update user)

--- Supermarket/Supermarket/src/main/java/com/Mathi/Supermarket/controller/UserController.java
+++ Supermarket/Supermarket/src/main/java/com/Mathi/Supermarket/controller/UserController.java
@@ -1,4 +1,35 @@
 package com.Mathi.Supermarket.controller;
 
+
+import com.Mathi.Supermarket.model.User;
+import com.Mathi.Supermarket.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/profile/{username}")
+    public ResponseEntity<?> getUserDetails(@PathVariable String username) {
+        return userService.getUserByUsername(username)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+
+    @PutMapping("/profile/{username}")
+    public ResponseEntity<?> updateUser(@PathVariable String username, @RequestBody User userDetails) {
+        try {
+            User updatedUser = userService.updateUser(username, userDetails);
+            return ResponseEntity.ok(updatedUser);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
