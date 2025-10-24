28a3bdd3c40d2fe8db61bfe2f14acefbc9cf039f
Kathisan <Arulmohankathi0407@gmail.com>
2025-10-24 20:25:37 +0530

    add MvcConfig to handle file upload resource mapping

--- Supermarket/Supermarket/src/main/java/com/Mathi/Supermarket/service/FileStorageService.java
+++ Supermarket/Supermarket/src/main/java/com/Mathi/Supermarket/service/FileStorageService.java
@@ -1,4 +1,44 @@
 package com.Mathi.Supermarket.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.UUID;
+
+@Service
 public class FileStorageService {
+
+    private final Path fileStorageLocation;
+
+    public FileStorageService() {
+        this.fileStorageLocation = Paths.get("./uploads").toAbsolutePath().normalize();
+
+        try {
+            Files.createDirectories(this.fileStorageLocation);
+        } catch (Exception ex) {
+            throw new RuntimeException("Could not create the directory where the uploaded files will be stored.", ex);
+        }
+    }
+
+    public String storeFile(MultipartFile file) {
+        // Generate a unique file name
+        String originalFileName = file.getOriginalFilename();
+        String uniqueFileName = UUID.randomUUID().toString() + "_" + originalFileName;
+
+        try {
+            // Copy file to the target location
+            Path targetLocation = this.fileStorageLocation.resolve(uniqueFileName);
+            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+
+            return uniqueFileName;
+        } catch (IOException ex) {
+            throw new RuntimeException("Could not store file " + uniqueFileName + ". Please try again!", ex);
+        }
+    }
 }
