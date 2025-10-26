a0fa04931fb72c89a7fa8ed7d214abb043fb80c0
LAHINTH <lahinthmurugiah@gmail.com>
2025-10-26 23:25:37 +0530

    Modified order service

--- Supermarket/Supermarket/src/main/java/com/Mathi/Supermarket/model/CustomerOrder.java
+++ Supermarket/Supermarket/src/main/java/com/Mathi/Supermarket/model/CustomerOrder.java
@@ -96,6 +96,22 @@ public class CustomerOrder {
         return orderDate;
     }
 
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
     public void setOrderItems(List<OrderItem> orderItems) {
         this.orderItems = orderItems;
     }
